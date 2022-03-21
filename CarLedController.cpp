1f29faa594aa459b0430ce97ccae9673cda88247
Michael Nutt <nuttmj@rose-hulman.edu>
2022-03-21 18:56:48 -0400

    dev

--- CarLedController.cpp
+++ CarLedController.cpp
@@ -89,3 +89,87 @@ void CarLedController::renderLoop() {
 void CarLedController::setLed(int row, int col, uint32_t color) {
     if(_leds[row*_numRows+col] >= 0) _strip.setPixelColor(_leds[row*_numRows+col], color);
 }
+
+void CarLedController::GAMER(int interval) {
+    int loopIterations = 361;
+    int i = (millis() / interval) % loopIterations;
+
+    float saturation = 1; // Between 0 and 1 (0 = gray, 1 = full color)
+    float brightness = 1; // Between 0 and 1 (0 = dark, 1 is full brightness)
+
+    for(int j = 0; j < _ledCount; j++) {
+        float hue = (i+j) % 361
+        long color = HSBtoRGB(hue, saturation, brightness); 
+        int red = color >> 16 & 255;
+        int green = color >> 8 & 255;
+        int blue = color & 255;
+        _strip.setPixelColor(j, _strip.Color(red, green, blue));
+    }
+}
+
+long HSBtoRGB(float _hue, float _sat, float _brightness) {
+    float red = 0.0;
+    float green = 0.0;
+    float blue = 0.0;
+    
+    if (_sat == 0.0) {
+        red = _brightness;
+        green = _brightness;
+        blue = _brightness;
+    } else {
+        if (_hue == 360.0) {
+            _hue = 0;
+        }
+
+        int slice = _hue / 60.0;
+        float hue_frac = (_hue / 60.0) - slice;
+
+        float aa = _brightness * (1.0 - _sat);
+        float bb = _brightness * (1.0 - _sat * hue_frac);
+        float cc = _brightness * (1.0 - _sat * (1.0 - hue_frac));
+        
+        switch(slice) {
+            case 0:
+                red = _brightness;
+                green = cc;
+                blue = aa;
+                break;
+            case 1:
+                red = bb;
+                green = _brightness;
+                blue = aa;
+                break;
+            case 2:
+                red = aa;
+                green = _brightness;
+                blue = cc;
+                break;
+            case 3:
+                red = aa;
+                green = bb;
+                blue = _brightness;
+                break;
+            case 4:
+                red = cc;
+                green = aa;
+                blue = _brightness;
+                break;
+            case 5:
+                red = _brightness;
+                green = aa;
+                blue = bb;
+                break;
+            default:
+                red = 0.0;
+                green = 0.0;
+                blue = 0.0;
+                break;
+        }
+    }
+
+    long ired = red * 255.0;
+    long igreen = green * 255.0;
+    long iblue = blue * 255.0;
+    
+    return long((ired << 16) | (igreen << 8) | (iblue));
+}
